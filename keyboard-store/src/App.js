de331d42ad3bfc10a3d7c09e42f7327f8e30c04a
andrew0149 <andrew-m0r0z@mail.ru>
2024-06-07 17:54:39 +0500

    Add react-bootstrap import and update meta info Import react-bootstrap for future usage Add favicon Update title and description

--- keyboard-store/src/App.js
+++ keyboard-store/src/App.js
@@ -1,11 +1,40 @@
 import './components/ProductOrderCard'
 import ProductOrderCard from './components/ProductOrderCard';
-
+import { Navbar, Container, } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="app"> 
-      <ProductOrderCard title='A' price='1000' description='Abcdef' image='' switches={new Map([['blue', 'Синие'], ['red', 'Красные']])}></ProductOrderCard>
+    <div className='app'>
+      <Navbar>
+        <Container>
+
+        </Container>
+      </Navbar>
+
+      <div className='wrapper'>
+        <div className='bestseller_carousel'>
+
+        </div>
+      </div>
+
+      <div className='wrapper'>
+        <div className='products_list'>
+          <ProductOrderCard title='A' price='1000' description='Abcdef' image='' switches={new Map([['blue', 'Синие'], ['red', 'Красные']])}></ProductOrderCard>
+        </div>
+      </div>
+
+
+      <div className='wrapper'>
+        <div className='reviews'>
+
+        </div>
+      </div>
+
+      <div className='wrapper'>
+        <div className='about_us'>
+
+        </div>
+      </div>
     </div>
   );
 }
